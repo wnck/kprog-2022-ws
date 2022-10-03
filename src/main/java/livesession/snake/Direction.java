4b59853c356b88bec7c085df792bd11d599fa9a1
Joerg Winckler <winckler@hs-heilbronn.de>
2022-10-03 05:34:54 +0200

    Direction now can calculate what is left and right.

--- src/main/java/livesession/snake/Direction.java
+++ src/main/java/livesession/snake/Direction.java
@@ -1,5 +1,28 @@
 package livesession.snake;
 
 public enum Direction {
-  NORTH, EAST, SOUTH, WEST
-}
+  NORTH, EAST, SOUTH, WEST;
+
+  private Direction left;
+  private Direction right;
+
+  static {
+    NORTH.left = WEST;
+    NORTH.right = EAST;
+    SOUTH.left = EAST;
+    SOUTH.right = WEST;
+    EAST.left = NORTH;
+    EAST.right = SOUTH;
+    WEST.left = SOUTH;
+    WEST.right = NORTH;
+  }
+
+  public Direction getLeft() {
+    return left;
+  }
+
+  public Direction getRight() {
+    return right;
+  }
+
+  }
