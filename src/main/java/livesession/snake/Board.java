eb559a98e653f28b9cdba109e2076d423d8c408e
Joerg Winckler <winckler@hs-heilbronn.de>
2022-10-05 09:08:51 +0200

    adding constant with minimal board size.

--- src/main/java/livesession/snake/Board.java
+++ src/main/java/livesession/snake/Board.java
@@ -1,6 +1,8 @@
 package livesession.snake;
 
 public interface Board {
+  int MINIMAL_BOARD_SIZE = 4;
+
   int size();
   BoardState getStateFromPosition(Coordinate coordinate);
   BoardState getStateFromPosition(int row, int column);
