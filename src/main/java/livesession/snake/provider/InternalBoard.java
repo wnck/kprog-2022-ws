5f6ca51b64a3dd3cebc735436ae25ffde4a535d2
Joerg Winckler <winckler@hs-heilbronn.de>
2022-10-06 10:32:24 +0200

    InternalBoard added.

---
+++ src/main/java/livesession/snake/provider/InternalBoard.java
@@ -0,0 +1,30 @@
+package livesession.snake.provider;
+
+import livesession.snake.BoardState;
+import livesession.snake.Coordinate;
+
+public class InternalBoard extends BaseBoard {
+  private static final org.slf4j.Logger logger =
+      org.slf4j.LoggerFactory.getLogger(InternalBoard.class);
+
+  public InternalBoard(final int size) {
+    super(size);
+
+    // TODO: Init board with GRASS and WALLs
+  }
+
+  public Coordinate getStartPosition() {
+    return new Coordinate(size / 2, size / 2);
+  }
+
+  protected void addFood(Coordinate coordinate) {
+    assertPositionIsOnBoard(coordinate);
+    board[coordinate.getRow()][coordinate.getColumn()] = BoardState.FOOD;
+  }
+
+  protected void removeFood(Coordinate coordinate){
+    assertPositionIsOnBoard(coordinate);
+    board[coordinate.getRow()][coordinate.getColumn()] = BoardState.GRASS;
+  }
+
+}
