59c5664e06533eb27b8c0e31dd31f5077a5ba6e7
Joerg Winckler <winckler@hs-heilbronn.de>
2022-10-10 14:50:45 +0200

    fixing checkstyle problems.

--- src/main/java/examples/ch14/newsfeedv1g/AddressBookDemo.java
+++ src/main/java/examples/ch14/newsfeedv1g/AddressBookDemo.java
@@ -4,54 +4,50 @@ package examples.ch14.newsfeedv1g;
  * Provide a simple demonstration of the AddressBook class.
  * Sample data is added to the address book,
  * and a GUI view is provided.
- * 
+ *
  * @author David J. Barnes and Michael Kölling.
  * @version 2016.02.29
  */
-public class AddressBookDemo
-{
-    private AddressBook book;
-    private AddressBookGUI interact;
+public class AddressBookDemo {
+  private AddressBook book;
+  private AddressBookGUI interact;
 
-    /**
-     * Setup an AddressBook with sample data.
-     * The address book is passed to a GUI to provide
-     * a view of the data.
-     */
-    public AddressBookDemo()
-    {
-        ContactDetails[] sampleDetails = {
-            new ContactDetails("david",   "08459 100000", "address 1"),
-            new ContactDetails("michael", "08459 200000", "address 2"),
-            new ContactDetails("john",    "08459 300000", "address 3"),
-            new ContactDetails("helen",   "08459 400000", "address 4"),
-            new ContactDetails("emma",    "08459 500000", "address 5"),
-            new ContactDetails("kate",    "08459 600000", "address 6"),
-            new ContactDetails("chris",   "08459 700000", "address 7"),
-            new ContactDetails("ruth",    "08459 800000", "address 8"),
-        };
-        book = new AddressBook();
-        for(ContactDetails details : sampleDetails) {
-            book.addDetails(details);
-        }
-
-        // Provide a GUI view of the address book.
-        interact = new AddressBookGUI(book);
+  /**
+   * Setup an AddressBook with sample data.
+   * The address book is passed to a GUI to provide
+   * a view of the data.
+   */
+  public AddressBookDemo() {
+    ContactDetails[] sampleDetails = {
+        new ContactDetails("david", "08459 100000", "address 1"),
+        new ContactDetails("michael", "08459 200000", "address 2"),
+        new ContactDetails("john", "08459 300000", "address 3"),
+        new ContactDetails("helen", "08459 400000", "address 4"),
+        new ContactDetails("emma", "08459 500000", "address 5"),
+        new ContactDetails("kate", "08459 600000", "address 6"),
+        new ContactDetails("chris", "08459 700000", "address 7"),
+        new ContactDetails("ruth", "08459 800000", "address 8"),
+    };
+    book = new AddressBook();
+    for (ContactDetails details : sampleDetails) {
+      book.addDetails(details);
     }
 
-    /**
-     * Allow the user to interact with the address book.
-     */
-    public void showInterface()
-    {
-        interact.setVisible(true);
-    }
+    // Provide a GUI view of the address book.
+    interact = new AddressBookGUI(book);
+  }
 
-    /**
-     * @return The sample address book.
-     */
-    public AddressBook getBook()
-    {
-        return book;
-    }
+  /**
+   * Allow the user to interact with the address book.
+   */
+  public void showInterface() {
+    interact.setVisible(true);
+  }
+
+  /**
+   * @return The sample address book.
+   */
+  public AddressBook getBook() {
+    return book;
+  }
 }
--- src/main/java/examples/ch14/newsfeedv1t/Parser.java
+++ src/main/java/examples/ch14/newsfeedv1t/Parser.java
@@ -9,59 +9,54 @@ import java.util.Scanner;
  * @author David J. Barnes and Michael Kölling.
  * @version 2016.02.29
  */
-public class Parser 
-{
-    // Hold all valid command words.
-    private CommandWords commands;
-    private Scanner reader;
+public class Parser {
+  // Hold all valid command words.
+  private CommandWords commands;
+  private Scanner reader;
 
-    public Parser() 
-    {
-        commands = new CommandWords();
-        reader = new Scanner(System.in);
-    }
+  public Parser() {
+    commands = new CommandWords();
+    reader = new Scanner(System.in);
+  }
 
-    /**
-     * Read the next command from the user.
-     * The returned command will be valid.
-     * @return A valid command.
-     */
-    public String getCommand() 
-    {
-        String command = null;
-        do {
-           // Print a prompt.
-            System.out.print("> ");
-            
-            String word = reader.next();
-            // Discard the rest of the line.
-            readLine();
-            if(commands.isCommand(word)) {
-                command = word;
-            }
-            else{
-                System.out.println("Unrecognized command: " + word);
-                System.out.print("Valid commands are: ");
-                commands.showAll();
-            }
-        } while(command == null);
-    
-        return command;
-    }
+  /**
+   * Read the next command from the user.
+   * The returned command will be valid.
+   *
+   * @return A valid command.
+   */
+  public String getCommand() {
+    String command = null;
+    do {
+      // Print a prompt.
+      System.out.print("> ");
 
-    /**
-     * Print out a list of valid command words.
-     */
-    public void showCommands()
-    {
+      String word = reader.next();
+      // Discard the rest of the line.
+      readLine();
+      if (commands.isCommand(word)) {
+        command = word;
+      } else {
+        System.out.println("Unrecognized command: " + word);
+        System.out.print("Valid commands are: ");
         commands.showAll();
-    }
+      }
+    } while (command == null);
 
-    /**
-     * @return A line of text from the user.
-     */
-    public String readLine()
-    {
-        return reader.nextLine();
-    }
+    return command;
+  }
+
+  /**
+   * Print out a list of valid command words.
+   */
+  public void showCommands() {
+    commands.showAll();
+  }
+
+  /**
+   * @return A line of text from the user.
+   */
+  public String readLine() {
+    return reader.nextLine();
+  }
 }
