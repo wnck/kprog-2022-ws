db359c2ccbcabc7c10fd6336a5745a3d3b964827
Joerg Winckler <winckler@hs-heilbronn.de>
2022-10-10 07:34:41 +0200

    Extending Reason with a message mode, usefull e.g. when the player aborts the game.

--- src/main/java/livesession/snake/Reason.java
+++ src/main/java/livesession/snake/Reason.java
@@ -6,17 +6,46 @@ public class Reason {
 
   private Coordinate coordinate;
   private BoardState state;
+  private String message;
+  private Mode mode;
 
   public Reason(final Coordinate coordinate, final BoardState state) {
     this.coordinate = coordinate;
     this.state = state;
+    mode = Mode.COORDINATE;
+  }
+
+  public Reason(final String message) {
+    this.message = message;
+    mode = Mode.MESSAGE;
   }
 
   public Coordinate getCoordinate() {
+    if (mode.equals(Mode.MESSAGE)) {
+      throw new IllegalStateException("Reason is in message mode.");
+    }
     return coordinate;
   }
 
   public BoardState getState() {
+    if (mode.equals(Mode.MESSAGE)) {
+      throw new IllegalStateException("Reason is in message mode.");
+    }
     return state;
+  }
+
+  public String getMessage() {
+    if (mode.equals(Mode.COORDINATE)) {
+      throw new IllegalStateException("Reason is in coordinate mode.");
+    }
+    return message;
+  }
+
+  public Mode getMode() {
+    return mode;
+  }
+
+  enum Mode {
+    MESSAGE, COORDINATE
   }
 }
