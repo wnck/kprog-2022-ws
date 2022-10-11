71afb0e2da46d2659bc7da34bd72ba815da0b0e0
Joerg Winckler <winckler@hs-heilbronn.de>
2022-10-11 08:55:45 +0200

    more tests for the SimpleSnake

--- src/test/java/livesession/snake/provider/TestSimpleSnake.java
+++ src/test/java/livesession/snake/provider/TestSimpleSnake.java
@@ -2,6 +2,7 @@ package livesession.snake.provider;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.List;
 import livesession.snake.BoardState;
@@ -19,10 +20,12 @@ public class TestSimpleSnake {
   FakeInternalBoard fakeBoard;
   FakeSnakeService fakeService;
   SimpleSnake snake;
+  private InternalBoard internalBoard;
 
   @Before
   public void setup() {
     fakeBoard = new FakeInternalBoard(DEFAULT_SIZE);
+    internalBoard = new InternalBoard(DEFAULT_SIZE);
     fakeService = new FakeSnakeService(fakeBoard);
     snake = new SimpleSnake(fakeService);
   }
@@ -32,7 +35,7 @@ public class TestSimpleSnake {
     Coordinate startPosition = fakeBoard.getStartPosition();
     List<Coordinate> snakePosition = snake.getPosition();
     assertEquals(snakePosition.get(0), startPosition);
-    assertEquals(snake.getDirection(), Direction.EAST);
+    assertEquals( Direction.EAST, snake.getDirection());
   }
 
   @Test @Ignore
@@ -65,5 +68,49 @@ public class TestSimpleSnake {
     assertEquals(foodPosition, snakeHead);
     assertEquals(snakeLength+1, snake.getPosition().size());
     assertTrue(fakeService.isEaten());
+  }
+
+  @Test @Ignore
+  public void checkAdvanceToWall() {
+    // prepare wall position
+    Coordinate startPosition = fakeBoard.getStartPosition();
+    int snakeLength = snake.getPosition().size();
+    Coordinate wallPosition = startPosition.getNeighbor(Direction.EAST);
+    fakeBoard.setPosition(wallPosition, BoardState.WALL);
+
+    // let the snake go one step (wiggle)
+    try {
+      Coordinate snakeHead = snake.advance();
+      fail("Snake should hit the wall and throw an IllegalPositionException.");
+    } catch (IllegalPositionException e) {
+      assertEquals(wallPosition, e.getCoordinate());
+      assertEquals(BoardState.WALL, e.getState());
+    }
+  }
+
+  @Test @Ignore
+  public void checkAdvanceToSnake() throws IllegalPositionException {
+    // prepare food position
+    Coordinate startPosition = fakeBoard.getStartPosition();
+    int snakeLength = snake.getPosition().size();
+    Coordinate foodPosition = startPosition.getNeighbor(Direction.EAST);
+    fakeBoard.setPosition(foodPosition, BoardState.FOOD);
+
+    // let the snake go one step (wiggle)
+    Coordinate snakeHead = snake.advance();
+
+    // Now lets turn the snake 180 degrees
+    snake.goLeft();
+    snake.goLeft();
+
+    try {
+      snake.advance();
+      fail("Snake should hit itself and throw an IllegalPositionException.");
+    } catch (IllegalPositionException e){
+      assertEquals(startPosition, e.getCoordinate());
+      assertEquals(BoardState.SNAKE, e.getState());
+    }
+
+
   }
 }
