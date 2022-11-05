450fba0aac69c8ae2e1a0bf4226b916d65f631a2
Joerg Winckler <winckler@hs-heilbronn.de>
2022-11-05 13:35:56 +0100

    adding all example.javafx packages to module info for execution.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,13 @@ module kprog.main{
     requires javafx.controls;
     requires javafx.fxml;
     exports environment.testprograms;
+    exports examples.javafx.bidirectional;
+    exports examples.javafx.firststeps;
+    exports examples.javafx.fxml;
+    exports examples.javafx.listcell;
+    exports examples.javafx.misc;
     exports examples.javafx.modal;
+    exports examples.javafx.mvvm;
+    exports examples.javafx.observable;
     exports prog.javafx.main;
     }
