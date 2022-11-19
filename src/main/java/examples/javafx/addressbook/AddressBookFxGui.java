fca2de060ed61aa80eb2c6b2803afb370a128bf8
Joerg Winckler <winckler@hs-heilbronn.de>
2022-11-19 18:48:04 +0100

    listEntries works also. Quick and not perfect.

--- src/main/java/examples/javafx/addressbook/AddressBookFxGui.java
+++ src/main/java/examples/javafx/addressbook/AddressBookFxGui.java
@@ -10,7 +10,6 @@ import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 public class AddressBookFxGui extends TabPane {
@@ -33,6 +32,7 @@ public class AddressBookFxGui extends TabPane {
     getTabs().add(newEntryTab);
     addNewEntry(newEntryTab);
     getTabs().add(listTab);
+    addListEntries(listTab);
   }
 
   private void addSearch(final Tab searchTab) {
@@ -55,31 +55,26 @@ public class AddressBookFxGui extends TabPane {
     contactDetailsObservableList.clear();
     contactDetailsObservableList.addAll(result);
   }
-private void addNewEntry(final Tab newEntryTab) {
-  VBox addEntryBox = new VBox();
-  addEntryBox.getChildren().add(new Label("Name"));
-  nameTextField = new TextField();
-  addEntryBox.getChildren().add(nameTextField);
-  addEntryBox.getChildren().add(new Label("Phone"));
-  phoneTextField = new TextField();
-  addEntryBox.getChildren().add(phoneTextField);
-  addEntryBox.getChildren().add(new Label("Address"));
-  addressTextArea = new TextArea();
-  addEntryBox.getChildren().add(addressTextArea);
-  Button addButton = new Button("Add");
-  addButton.setOnAction((e) -> addEntryToBook());
-  Button clearButton = new Button("Clear");
-  clearButton.setOnAction((e) -> clearEntryForm());
-  HBox buttonBox = new HBox();
-  buttonBox.getChildren().addAll(addButton, clearButton);
-  addEntryBox.getChildren().add(buttonBox);
-  newEntryTab.setContent(addEntryBox);
-}
 
-  private void clearEntryForm() {
-    nameTextField.textProperty().setValue("");
-    phoneTextField.textProperty().setValue("");
-    addressTextArea.textProperty().setValue("");
+  private void addNewEntry(final Tab newEntryTab) {
+    VBox addEntryBox = new VBox();
+    addEntryBox.getChildren().add(new Label("Name"));
+    nameTextField = new TextField();
+    addEntryBox.getChildren().add(nameTextField);
+    addEntryBox.getChildren().add(new Label("Phone"));
+    phoneTextField = new TextField();
+    addEntryBox.getChildren().add(phoneTextField);
+    addEntryBox.getChildren().add(new Label("Address"));
+    addressTextArea = new TextArea();
+    addEntryBox.getChildren().add(addressTextArea);
+    Button addButton = new Button("Add");
+    addButton.setOnAction((e) -> addEntryToBook());
+    Button clearButton = new Button("Clear");
+    clearButton.setOnAction((e) -> clearEntryForm());
+    HBox buttonBox = new HBox();
+    buttonBox.getChildren().addAll(addButton, clearButton);
+    addEntryBox.getChildren().add(buttonBox);
+    newEntryTab.setContent(addEntryBox);
   }
 
   private void addEntryToBook() {
@@ -87,8 +82,31 @@ private void addNewEntry(final Tab newEntryTab) {
         nameTextField.getText(),
         phoneTextField.getText(),
         addressTextArea.getText()
-        );
+    );
     book.addDetails(contactDetails);
   }
+
+  private void clearEntryForm() {
+    nameTextField.textProperty().setValue("");
+    phoneTextField.textProperty().setValue("");
+    addressTextArea.textProperty().setValue("");
+  }
+
+  private void addListEntries(final Tab listTab) {
+    VBox listBox = new VBox();
+    ObservableList<ContactDetails> completeContactDetails = FXCollections.observableArrayList();
+    ListView<ContactDetails> contactDetailsListView = new ListView<>();
+    contactDetailsListView.setItems(completeContactDetails);
+    listBox.getChildren().add(contactDetailsListView);
+    HBox buttonBox = new HBox();
+    Button listButton = new Button("List");
+    listButton.setOnAction((e) -> {completeContactDetails.clear(); completeContactDetails.addAll(book.search(""));});
+    Button clearButton = new Button("Clear");
+    clearButton.setOnAction((e) -> completeContactDetails.clear());
+    buttonBox.getChildren().addAll(listButton, clearButton);
+    listBox.getChildren().add(buttonBox);
+    listTab.setContent(listBox);
+  }
+
 
 }
