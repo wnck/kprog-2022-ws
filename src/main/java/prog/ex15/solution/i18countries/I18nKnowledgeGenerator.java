ca47520588249c20aa648a32eac40e22e7c53af8
Joerg Winckler <winckler@hs-heilbronn.de>
2022-12-23 10:18:08 +0100

    fixing checkstyle problem with class name

--- src/main/java/prog/ex15/solution/i18countries/I18nKnowledgeGenerator.java
+++ src/main/java/prog/ex15/solution/i18countries/I18nKnowledgeGenerator.java
@@ -7,9 +7,9 @@ import prog.ex15.exercise.i18ncountries.KnowledgeGenerator;
  * Simple, straight-forward implementation of the KnowledgeGenerator interface for multiple
  * countries.
  */
-public class I18NKnowledgeGenerator implements KnowledgeGenerator {
+public class I18nKnowledgeGenerator implements KnowledgeGenerator {
   private static final org.slf4j.Logger logger =
-          org.slf4j.LoggerFactory.getLogger(I18NKnowledgeGenerator.class);
+          org.slf4j.LoggerFactory.getLogger(I18nKnowledgeGenerator.class);
 
   @Override
   public CountryKnowledgeContainer fillContainer() {
